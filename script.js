8772b9bf28de7ba200fb62d41aca5b38c0ae9d24
GRND0 <zefiro.turchetta@gmail.com>
2022-05-05 14:30:24 +0200

    aggiunto bonus

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var app = new Vue (
         el: '#root',
         data: {
             messaggio : 'Benvenuto in VUE',
+            immagine : 'https://picsum.photos/id/1/1000/700' ,
         }
     }
 )
